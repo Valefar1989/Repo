83b153c0adc7dd748cc7864355078a8517fffc7d
Valefar1989 <eliseev@spartak.ru>
2023-06-23 08:33:47 +0300

    Домашние задание 2
    Домашнее задание 2

---
+++ python/PythonSpecial/DZ2/dz2.2.py
@@ -0,0 +1,19 @@
+#Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление. 
+# Функцию hex используйте для проверки своего результата.
+
+
+number = int(input('Введите число:'))
+base = 16
+
+# перевод в 16 систему
+def convert_to(number, base, upper=False):
+    digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+    if base > len(digits): return None
+    result = ''
+    while number > 0:
+        result = digits[number % base] + result
+        number //= base
+    return result.upper() if upper else result
+
+print(convert_to(number, base))
+print(hex(number)[2:])
---
+++ python/PythonSpecial/DZ2/dz2.3.py
@@ -0,0 +1,30 @@
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
+
+drob_a ='1/3'
+drob_b ='2/5'
+
+# Превращаем строку в список по разделителю
+def razdel(drob):
+    dr_l = drob.split('/')
+    return dr_l
+
+# Сложение
+def slozhenie(drob_ls_1, drob_ls_2):
+    znam = int(drob_ls_1[1])*int(drob_ls_2[1])
+    cheslit = (int(drob_ls_1[0])*int(drob_ls_2[1]))+(int(drob_ls_2[0])*int(drob_ls_1[1]))
+    new_drob = str(cheslit)+'/'+str(znam)
+    return new_drob
+
+# Произведение
+def mult(drob_ls_1, drob_ls_2):
+    znam = int(drob_ls_1[1])*int(drob_ls_2[1])
+    cheslit = int(drob_ls_1[0])*int(drob_ls_2[0])
+    new_drob = str(cheslit)+'/'+str(znam)
+    return new_drob
+
+dr_l_a = razdel(drob_a)
+dr_l_b = razdel(drob_b)
+
+print(slozhenie(dr_l_a, dr_l_b))
+print(mult(dr_l_a, dr_l_b))
